912d8412c2db4f2843921c20e0b81ecd8f3ed6da
Goldie323 <Goldieross323@gmail.com>
2025-11-27 17:51:46 -0500

    Got multithreading fully to work. Need to parse :C

--- ThreadedTicks.c
+++ ThreadedTicks.c
@@ -1,5 +1,6 @@
 #include <threads.h>
 #include <stdatomic.h>
+#include <stdio.h>
 #include "Consts.h"
 #include "GateFuncs.h"
 #include "Util.h"
@@ -7,63 +8,77 @@
 
 thrd_t threads[MAX_THREADS];
 bool intialized = false;
-_Atomic block *threadWork[MAX_THREADS];
-atomic_uint_fast8_t ActiveCount = 0;
+volatile block *threadWork[MAX_THREADS];
+volatile bool writeTurn[MAX_THREADS]; //decides if it's the threads turn to write or Mains turn to write. Both can read all the time. This is just to avoid possible byte ordering issues.
+volatile uint_fast8_t activeCount = 0;
 
 atomic_uint_fast64_t workingCount = 0;
-atomic_bool work = false;
-atomic_bool terminateThreads = false;
-atomic_bool globalFlipBit = false;
+volatile bool work = false;
+volatile bool terminateThreads = false;
+volatile bool globalFlipBit = false;
 int workerThread(void *arg) {
     uint_fast8_t threadID = (uint_fast8_t)(uintptr_t)arg;
     bool localWork = false;
     while (true) {
-        while (!atomic_load(&work) && !atomic_load(&terminateThreads)) {
-            if (localWork&&!atomic_load(&work)) {
+        while (!(work || terminateThreads)) {
+            if (localWork&&!work) {
                 atomic_fetch_sub(&workingCount, 1);
                 localWork = false;
             }
             thrd_yield();
         }
-        if (!localWork&&atomic_load(&work)) {
-            atomic_fetch_add(&workingCount, 1);
-            localWork = true;
-        }
         
-        if ((atomic_load(&ActiveCount) == 0) || (atomic_load(&terminateThreads) && threadID >= atomic_load(&ActiveCount)-1)) {
+        if (terminateThreads && ((activeCount == 0) || (threadID >= activeCount-1))) {
             return 0;
         }
         
-        while (atomic_load(&work)) {
-            _Atomic block *b = atomic_load(&threadWork[threadID]); //main writes to and then this thread notices it and will set it to NULL afterwards so that main knows to write to it again.
-            atomic_store(&threadWork[threadID], NULL);
-            if (!b) {
-                thrd_yield();
-                continue;
-            }
+        while (work) {
+            localWork = true;
+            if (writeTurn[threadID]) { thrd_yield(); continue; }
+            block *b = (block *)threadWork[threadID]; //main writes to and then this thread notices it and will set it to NULL afterwards so that main knows to write to it again.
+            threadWork[threadID] = NULL;
+            if (!b) { thrd_yield(); continue; }
             computeBlock((block *)b, atomic_load(&globalFlipBit));
+            writeTurn[threadID] = false;
         }
     }
 }
 
 static inline bool insertWorkFirstAvailable(block *b) {
-    for (uint_fast8_t i = 0; i < atomic_load(&ActiveCount); i++) {
-        if (!atomic_load(&threadWork[i])) {
-            atomic_store(&threadWork[i], (_Atomic block *)b);
+    for (uint_fast8_t i = 0; i < activeCount; i++) {
+        if (!writeTurn[i]) {
+            threadWork[i] = b;
             return 1;
         }
     }
     return 0;
 }
 
+static inline bool allWorkNull() {
+    for (uint_fast8_t i = 0; i < activeCount; i++) {
+        //printf("POINTER[%lu]: %lu\n", i, (unsigned long)threadWork[i]);
+        if (threadWork[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
 void threadedTickCalc(block *list, bool flipBit) {
-    atomic_store(&globalFlipBit, flipBit);
-    work = true;
+    globalFlipBit = flipBit;
     block *b = list;
-    while (b != NULL) {
-        while (!insertWorkFirstAvailable(b)); //keep trying to put the value in until there's a free space.
+    for (uint_fast8_t i = 0; i < activeCount; i++) {
+        if (!b) break;
+        threadWork[i] = b;
         b = b->next;
     }
+    atomic_store(&workingCount, activeCount);
+    work = true;
+    while (b) {
+        while (!insertWorkFirstAvailable(b)) thrd_yield(); //keep trying to put the value in until there's a free space.
+        b = b->next;
+    }
+    while (!allWorkNull()) thrd_yield();
     work = false;
     while (atomic_load(&workingCount));
 }
@@ -77,15 +92,15 @@ void tickCalc(block *list, bool flipBit) {
 }
 
 void tick(block *list, bool flipBit) {
-    if (ActiveCount > 0) threadedTickCalc(list, flipBit);
+    if (activeCount > 0) threadedTickCalc(list, flipBit);
     else tickCalc(list, flipBit);
 }
 
 void killThreads(uint_fast8_t num) {
-    ActiveCount -= num;
+    activeCount -= num;
     terminateThreads = true;
     for (uint_fast8_t i = 0; i < num; i++) {
-        thrd_join(threads[ActiveCount+i], NULL);
+        thrd_join(threads[activeCount+i], NULL);
     }
     terminateThreads = false;
     return;
@@ -93,17 +108,20 @@ void killThreads(uint_fast8_t num) {
 
 void createThreads(uint_fast8_t num) {
     for (uint_fast8_t i = 0; i < num; i++) {
-        thrd_create(&threads[ActiveCount], workerThread, (void *)(uintptr_t)ActiveCount);
-        ActiveCount++;
+        thrd_create(&threads[activeCount], workerThread, (void *)(uintptr_t)activeCount);
+        activeCount++;
     }
 }
 
 void setThreadCount(uint_fast8_t count) {
-    if (!intialized) memset(threadWork, 0, sizeof(block *) * MAX_THREADS);
+    if (!intialized) {
+        memset(threadWork, 0, sizeof(block *) * MAX_THREADS);
+        memset((bool *)writeTurn, 0, sizeof(bool) * MAX_THREADS);
+    }
     if (count > MAX_THREADS) count = MAX_THREADS;
-    if (ActiveCount == count) return;
-    if (ActiveCount < count) createThreads(count-ActiveCount);
-    if (ActiveCount > count) killThreads(ActiveCount-count);
+    if (activeCount == count) return;
+    if (activeCount < count) createThreads(count-activeCount);
+    if (activeCount > count) killThreads(activeCount-count);
 }
 
 static inline void killAllThreads() {
