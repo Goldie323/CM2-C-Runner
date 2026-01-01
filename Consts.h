dc3fb4b71f9fa1b2129bcec31c2c8a6fdaa2008a
Goldie323 <Goldieross323@gmail.com>
2025-12-31 19:12:40 -0500

    made a union with extra data to save space

--- Consts.h
+++ Consts.h
@@ -31,77 +31,93 @@ enum {
     MAX_THREADS = 20 //should be set to twice the number of cores on your computer. This will probably be set up to a reasonable number later on but this is just to prevent dynamic memory for a simple array of the threads.
 };
 
-#define ID_MASK     0x1F  // 0001 1111
-#define BOOL0_MASK  0x20  // 0010 0000
-#define BOOL1_MASK  0x40  // 0100 0000
-
-typedef struct block{
-    struct block **inputs;
-    unsigned long int inputsSize;
-    unsigned long int inputCount;
-    struct block **outputs;
-    unsigned long int outputsSize;
-    unsigned long int outputCount;
-    unsigned long int x, y, z;
-    struct block *next;
-    uint_least8_t OwnerID;
-    uint_least8_t meta;
-} block;
-
-typedef struct {
-    block Base;
+struct flipFlopBlock{
+    //ID 5
+    uint8_t a;
+    uint8_t b;
     bool PrevXor;
-} flipFlopBlock;
+};
 
-typedef struct {
-    block Base; //ID 6
+struct ledBlock{
+    //ID 6
     uint_least8_t RedAmount;
     uint_least8_t GreenAmount;
     uint_least8_t BlueAmount;
     uint_least8_t OpacityOn; // 5-100
     uint_least8_t OpacityOff; // 5-100
     bool analog;
-} ledBlock;
+};
 
-typedef struct {
-    block Base; //ID 7
+struct soundBlock{
+    //ID 7
     uint_least32_t Frequency; // fixed point 0-16000 with 2 decimal places
     uint_least8_t Instrument; // 0-5 for Sine, Square, Triangle Sawtooth, Meow, or Snare.
-} soundBlock;
+};
 
-typedef struct {
-    block Base; //ID 12
+struct randomBlock{
+    //ID 12
     uint_least8_t Probability; //in percent 0-100
-} randomBlock;
+};
 
-typedef struct {
-    block Base; //ID 13
+struct charBlock{
+    //ID 13
     char Character;
-} charBlock;
+};
 
-typedef struct {
-    block Base; //ID 14
+struct tileBlock{
+    //ID 14
     uint_least8_t RedAmount;
     uint_least8_t GreenAmount;
     uint_least8_t BlueAmount;
     uint_least8_t Material; // 0-13 for different material
     bool Collision;
-} tileBlock;
+};
 
-typedef struct {
-    block Base; //ID 16
+struct delayBlock{
+    //ID 16
     uint_least16_t DelayTime; // in ticks, 1-1000
-} delayBlock; 
+}; 
 
-typedef struct {
-    block Base; //ID 17
+struct antennaBlock{
+    //ID 17
     uint_least16_t Signal;
     bool Global;
-} antennaBlock;
+};
+
+struct ledMixerBlock{
+    //ID 19
+    bool Additive; // additive, subtractive blending
+}; 
+
+union extraData{
+    struct flipFlopBlock flipFlopBlock;
+    struct ledBlock ledBlock;
+    struct soundBlock soundBlock;
+    struct randomBlock randomBlock;
+    struct charBlock charBlock;
+    struct tileBlock tileBlock;
+    struct delayBlock delayBlock;
+    struct antennaBlock antennaBlock;
+    struct ledMixerBlock ledMixerBlock;
+};
+
+#define ID_MASK     0x1F  // 0001 1111
+#define BOOL0_MASK  0x20  // 0010 0000
+#define BOOL1_MASK  0x40  // 0100 0000
 
 typedef struct {
-    block Base; //ID 19
-    bool Additive; // additive, subtractive blending
-} ledMixerBlock; 
+    unsigned long int *inputs;
+    unsigned long int inputsSize;
+    unsigned long int inputCount;
+    unsigned long int *outputs;
+    unsigned long int outputsSize;
+    unsigned long int outputCount;
+    unsigned long int x, y, z;
+    union extraData;
+    uint_least8_t OwnerID;
+    uint_least8_t meta;
+} block;
+
+
 
 #endif
